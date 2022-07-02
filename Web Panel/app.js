76b78ea7cb08127133b59f2828cea0d8e3f99d7c
drkathirpi <85863904+drkathirpi@users.noreply.github.com>
2022-07-02 22:41:26 +0530

    Update app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -1,16 +1,11 @@
-///Edit Under This only
-
-  
-    var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
-  }; 
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyb_eCDzCcYb_q9FXMJRJDrpZLkdnK7AM",
+  authDomain: "myproject-f3bac.firebaseapp.com",
+  databaseURL: "https://myproject-f3bac-default-rtdb.firebaseio.com",
+  projectId: "myproject-f3bac",
+  storageBucket: "myproject-f3bac.appspot.com",
+  messagingSenderId: "29013780343",
+  appId: "1:29013780343:web:895000f4291cb41fc32372",
+  measurementId: "G-LYXP78XGRB"
+};
